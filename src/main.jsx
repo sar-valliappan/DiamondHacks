4120008f3f39d2b8ee84256510c826fb21c6a2b2
sar-valliappan <173072045+sar-valliappan@users.noreply.github.com>
2026-04-04 15:48:16 -0700

    Vite setup

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import PriceFinder from './Pricefinder'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <PriceFinder />
+  </React.StrictMode>
+)
