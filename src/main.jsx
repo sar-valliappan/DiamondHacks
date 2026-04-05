a3286636d345de234b88b10c6af93dbc62a3edf7
sar-valliappan <173072045+sar-valliappan@users.noreply.github.com>
2026-04-04 17:01:49 -0700

    removed everything for pivot

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import PriceFinder from './Pricefinder'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <PriceFinder />
-  </React.StrictMode>
-)
